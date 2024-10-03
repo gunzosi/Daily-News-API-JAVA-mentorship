72869e8adef27afcc397cb0da32194a4fb35fdaf
Dang Dai Duong <127579534+gunzosi@users.noreply.github.com>
2024-10-03 20:43:15 +0700

    UPDATE FINISH DAILY NEW RSS

---
+++ DailyNew/src/main/java/code/mentor/dto/RssRequest.java
@@ -0,0 +1,23 @@
+package code.mentor.dto;
+
+public class RssRequest {
+    private String rssUrl;
+    private String categoryName;
+
+    // Getter và Setter
+    public String getRssUrl() {
+        return rssUrl;
+    }
+
+    public void setRssUrl(String rssUrl) {
+        this.rssUrl = rssUrl;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
